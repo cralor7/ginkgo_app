dcacfb8f05b74bbc23ac881047266a75c2aca243
fyz <969563641@qq.com>
2018-08-24 08:53:01 +0800

    更新

--- ginkgo_app/app/src/main/java/com/qk/Constant.java
+++ ginkgo_app/app/src/main/java/com/qk/Constant.java
@@ -9,16 +9,13 @@ public class Constant {
     /**
      * 服务器地址
      */
-<<<<<<< HEAD
-  //  private static String server_url="http://10.2.72.112:5000/ginkgo/";
+
+     private static String server_url="http://10.2.72.112:5000/ginkgo/";
     //private static String server_url="http://10.2.72.155:8080/app_server/";
 
-    private static String server_url="http://10.2.72.112:8761/";
-=======
-    private static String server_url="http://10.2.72.112:5000/ginkgo/";
-//    private static String server_url="http://10.2.72.155:8080/app_server/";
 //    private static String server_url="http://10.2.72.112:8761/";
->>>>>>> 4b1c78b6f1d0f6be06e229747355b9a7ace773d3
+
+
 
     /**
      * 检查token是否有效接口
